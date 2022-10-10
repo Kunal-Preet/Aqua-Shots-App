8c73795b5209e9096d1cc3951cb6176bad212626
Kunal Preet <Kunalsingh8729@gmail.com>
2022-10-10 16:45:17 -0400

    Basic UI/UX

--- app/src/main/java/kunal/example/aquashots/MainUserScreen.kt
+++ app/src/main/java/kunal/example/aquashots/MainUserScreen.kt
@@ -5,22 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var enterBtn: Button
+class MainUserScreen : AppCompatActivity(), View.OnClickListener {
 
+    lateinit var settingsImgView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_user_screen)
 
-        enterBtn = findViewById(R.id.enterBtn)
-        enterBtn.setOnClickListener(this)
+        settingsImgView = findViewById(R.id.settingImgView)
+        settingsImgView.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         when (view?.id) {
-            R.id.enterBtn -> {
-                val intent = Intent(this, MainUserScreen::class.java).apply {  }
+            R.id.settingImgView -> {
+                val intent = Intent(this, SettingsActivity::class.java).apply {  }
                 startActivity(intent)
             }
         }
---
+++ app/src/main/java/kunal/example/aquashots/UserInfo.kt
@@ -0,0 +1,7 @@
+package kunal.example.aquashots
+
+import java.text.DateFormat
+
+data class UserInfo(var gender:String = "", var weight:Int = 0, var active:String = "", val dateFormat:String="")
+{
+}
